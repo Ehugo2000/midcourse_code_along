f31d301656f6690ab1f4bf2247269e92becdcf85
Ehugo2000 <erik.hgsn@gmail.com>
2020-12-03 14:57:36 +0100

    SEarchfunction working! c q exit quit
    sdf
    asdf

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -5,17 +5,22 @@ import axios from "axios";
 class GHSearch extends Component {
   state = {
     searchValue: "",
-    gitHubUsers: []
+    gitHubUsers: [],
   };
   setInputValue(event) {
     this.setState({ searchValue: event.target.value });
   }
   async performSearch(event) {
-    let q = this.state.searchValue
-    let response = await axios.get(`https://api.github.com/search/users?q=${q}`)
-    this.setState({gitHubUsers: response.data.items})
+    let q = this.state.searchValue;
+    let response = await axios.get(
+      `https://api.github.com/search/users?q=${q}`
+    );
+    this.setState({ gitHubUsers: response.data.items });
   }
   render() {
+    let displayUsers = this.state.gitHubUsers.map((user) => {
+      return <li key={user.id}> {user.login}</li>;
+    });
     return (
       <>
         <Input
@@ -31,6 +36,7 @@ class GHSearch extends Component {
           Search
         </Button>
         <div data-cy="search_results"></div>
+        <ul>{displayUsers}</ul>
       </>
     );
   }
